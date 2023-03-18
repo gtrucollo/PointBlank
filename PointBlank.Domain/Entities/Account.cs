88d16eca53045868732d5f90564e417e546dda0c
Gabriel Trucollo <gabrieltrucollo@gmail.com>
2023-03-18 16:31:46 -0300

    fix: enum values

--- PointBlank.Domain/Entities/Account.cs
+++ PointBlank.Domain/Entities/Account.cs
@@ -15,7 +15,7 @@ public class Account : Entity
     {
         Inactive = 0,
         Active = 1,
-        Suspended = 1,
+        Suspended = 2,
     }
 
     public StatusEnum Status { get; set; }
